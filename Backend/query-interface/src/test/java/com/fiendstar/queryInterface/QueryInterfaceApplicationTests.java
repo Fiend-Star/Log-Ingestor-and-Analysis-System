dd98939f55ba584074322b2e9008d74428587fd9
Gursimran <gurmsi1060@outlook.com>
2023-11-18 03:41:10 +0530

    folder Structure with semi working docker compose

---
+++ Backend/query-interface/src/test/java/com/fiendstar/queryInterface/QueryInterfaceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fiendstar.queryInterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class QueryInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
