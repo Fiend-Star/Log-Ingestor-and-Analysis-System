99c7de3d038b9b644f26212a2b532a96365236e6
Gursimran <gurmsi1060@outlook.com>
2023-11-18 23:08:03 +0530

    made elasticsearch healthy

--- init_scylla.sh
+++ init_scylla.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
 echo 1048576 > /proc/sys/fs/aio-max-nr
+
 scylla_io_setup
-exec /usr/bin/scylla --developer-mode=1
+
+/usr/bin/scylla --developer-mode=1
+
+until cqlsh -e "describe cluster"
+do
+    echo "Waiting for ScyllaDB to be ready..."
+    sleep 10
+done
+
+# Create keyspace
+cqlsh -e "CREATE KEYSPACE IF NOT EXISTS mykeyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};"
+
  #--smp 2 --memory 4G
 #--overprovisioned 1
 #--developer-mode=1
