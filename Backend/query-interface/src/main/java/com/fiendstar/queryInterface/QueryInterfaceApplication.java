c05833c7cdba195561eb8c2e01b5288b257ce2be
Gursimran <gurmsi1060@outlook.com>
2023-11-18 15:47:47 +0530

    initial commit of folder structure

---
+++ Backend/query-interface/src/main/java/com/fiendstar/queryInterface/QueryInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.fiendstar.queryInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryInterfaceApplication.class, args);
+    }
+
+}
